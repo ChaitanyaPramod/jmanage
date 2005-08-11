fc92fdcb21ada34424cf83d7e58af62e416d59d3
nobody <nobody@ecb09e13-aa40-0410-87da-d21392614d8a>
2005-08-11 01:49:09 +0000

    This commit was manufactured by cvs2svn to create tag 'jmanage-0-5-4'.

---
+++ tags/jmanage-0-5-4/main/src/org/jmanage/core/config/ConfigReader.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright 2004-2005 jManage.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmanage.core.config;
+
+import org.jdom.input.SAXBuilder;
+import org.jdom.JDOMException;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jmanage.core.crypto.Crypto;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ *
+ * Date: Jun 19, 2004
+ * @author  Shashank
+ */
+public class ConfigReader implements ConfigConstants{
+
+    /*  Single instance */
+    private static ConfigReader configReader =
+            new ConfigReader(new File(DEFAULT_CONFIG_FILE_NAME));
+    /*  Last modified time of the configuration file    */
+    private static long lastModified = -1;
+    /*  Cache the configuration file    */
+    private static Document config = null;
+
+    /**
+     * Initilizations done here.
+     *
+     * @param configFile
+     */
+    private ConfigReader(File configFile){
+        try{
+            lastModified = configFile.lastModified();
+            config = new SAXBuilder().build(configFile);
+        }catch(JDOMException jdEx){
+            System.out.println("Error reading config file " +
+                    DEFAULT_CONFIG_FILE_NAME);
+            jdEx.printStackTrace();
+        }
+    }
+
+    /**
+     * Invalidate cached information about configured applciations if the
+     * configuration file got updated after last read.
+     *
+     * @return
+     */
+    public static ConfigReader getInstance(){
+        File configFile = new File(DEFAULT_CONFIG_FILE_NAME);
+        if(lastModified < configFile.lastModified()){
+            /*  Refresh the cache   */
+            configReader = new ConfigReader(configFile);
+        }
+        return configReader;
+    }
+
+    /**
+     * To retrieve the details of all configured applications.
+     */
+    public List read(){
+
+        List applications =
+                config.getRootElement().getChild(APPLICATIONS).getChildren();
+        return getApplicationConfigList(applications, null);
+    }
+
+
+    private List getApplicationConfigList(List applications,
+                                          ApplicationConfig clusterConfig){
+
+        final List applicationConfigList = new LinkedList();
+        for(Iterator appIterator = applications.iterator(); appIterator.hasNext();){
+            Element application = (Element)appIterator.next();
+            ApplicationConfig appConfig = null;
+            if(APPLICATION.equalsIgnoreCase(application.getName())){
+                appConfig = getApplicationConfig(application);
+                appConfig.setClusterConfig(clusterConfig);
+            }else if(APPLICATION_CLUSTER.equals(application.getName())){
+                assert clusterConfig == null: "found cluster within a cluster";
+                appConfig = getApplicationClusterConfig(application);
+            }else{
+                assert false:"Invalid element:" + application.getName();
+            }
+            applicationConfigList.add(appConfig);
+        }
+        return applicationConfigList;
+    }
+
+    private ApplicationConfig getApplicationConfig(Element application){
+
+        List params = application.getChildren(PARAMETER);
+        Iterator paramIterator = params.iterator();
+        Map paramValues = new HashMap(1);
+        while(paramIterator.hasNext()){
+            Element param = (Element)paramIterator.next();
+            paramValues.put(param.getChildTextTrim(PARAMETER_NAME),
+                    param.getChildTextTrim(PARAMETER_VALUE));
+        }
+
+        final String encryptedPassword =
+                application.getAttributeValue(PASSWORD);
+        String password = null;
+        if(encryptedPassword != null){
+            password = Crypto.decrypt(encryptedPassword);
+        }
+        Integer port = null;
+        if(application.getAttributeValue(PORT) != null){
+            port = new Integer(application.getAttributeValue(PORT));
+        }
+        ApplicationConfig config =
+                ApplicationConfigFactory.create(
+                        application.getAttributeValue(APPLICATION_ID),
+                        application.getAttributeValue(APPLICATION_NAME),
+                        application.getAttributeValue(APPLICATION_TYPE),
+                        application.getAttributeValue(HOST),
+                        port,
+                        application.getAttributeValue(URL),
+                        application.getAttributeValue(USERNAME),
+                        password,
+                        paramValues);
+
+        config.setMBeans(getMBeanConfigList(application));
+        return config;
+    }
+
+    private ApplicationConfig getApplicationClusterConfig(Element application){
+
+        ApplicationClusterConfig config =
+                new ApplicationClusterConfig(
+                        application.getAttributeValue(APPLICATION_ID),
+                        application.getAttributeValue(APPLICATION_NAME));
+
+        if(application.getChild(APPLICATIONS) != null){
+            List applications =
+                    application.getChild(APPLICATIONS).getChildren();
+            List appConfigList = getApplicationConfigList(applications, config);
+            config.addAllApplications(appConfigList);
+        }
+
+        config.setMBeans(getMBeanConfigList(application));
+        return config;
+    }
+
+    private List getMBeanConfigList(Element application){
+        /* read mbeans */
+        List mbeanConfigList = new LinkedList();
+        if(application.getChild(MBEANS) != null){
+            List mbeans = application.getChild(MBEANS).getChildren(MBEAN);
+            for(Iterator it=mbeans.iterator(); it.hasNext(); ){
+                Element mbean = (Element)it.next();
+                MBeanConfig mbeanConfig =
+                        new MBeanConfig(mbean.getAttributeValue(MBEAN_NAME),
+                                mbean.getChildTextTrim(MBEAN_OBJECT_NAME));
+                mbeanConfigList.add(mbeanConfig);
+            }
+        }
+        return mbeanConfigList;
+    }
+}
---
+++ tags/jmanage-0-5-4/main/src/org/jmanage/core/util/CoreUtils.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2004-2005 jManage.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmanage.core.util;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.jmanage.core.management.ServerConnection;
+import org.jmanage.core.config.ApplicationConfig;
+
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+
+/**
+ *
+ * date:  Jun 22, 2004
+ * @author	Rakesh Kalra
+ */
+public class CoreUtils {
+
+    private static final Logger logger = Loggers.getLogger(CoreUtils.class);
+
+    public static String getRootDir(){
+        return System.getProperty(SystemProperties.JMANAGE_ROOT);
+    }
+
+    public static String getConfigDir(){
+        return getRootDir() + "/config";
+    }
+
+    public static String getWebDir(){
+        return getRootDir() + "/web";
+    }
+
+    public static String getModuleDir(String moduleId){
+        return getRootDir() + "/modules/" + moduleId;
+    }
+
+    public static String getLogDir(){
+        return getRootDir() + "/logs";
+    }
+
+
+    public static void copyProperties(Object dest, Object source) {
+        try {
+            BeanUtils.copyProperties(dest, source);
+        }
+        catch(Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static Object getTypedValue(ApplicationConfig appConfig,
+                                       String value,
+                                       String type){
+
+        if(type.equals("int")){
+            type = "java.lang.Integer";
+        }else if(type.equals("long")){
+            type = "java.lang.Long";
+        }else if(type.equals("short")){
+            type = "java.lang.Short";
+        }else if(type.equals("float")){
+            type = "java.lang.Float";
+        }else if(type.equals("double")){
+            type = "java.lang.Double";
+        }else if(type.equals("char")){
+            type = "java.lang.Character";
+        }else if(type.equals("boolean")){
+            type = "java.lang.Boolean";
+        }else if(type.equals("byte")){
+            type = "java.lang.Byte";
+        }
+
+        try {
+            /* handle ObjectName as a special type */
+            if(type.equals("javax.management.ObjectName")){
+                Class clazz = Class.forName(type, true,
+                        appConfig.getModuleClassLoader());
+                try {
+                    Constructor ctor = clazz.getConstructor(new Class[]{String.class});
+                    return ctor.newInstance(new Object[]{value});
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            /* other types */
+            return ConvertUtils.convert(value, Class.forName(type));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Object[] getTypedArray(ApplicationConfig appConfig,
+                                         String[] values,
+                                         String[] type){
+        Object[] obj = new Object[values.length];
+        for(int i=0; i<values.length; i++){
+            obj[i] = getTypedValue(appConfig, values[i], type[i]);
+        }
+        return obj;
+    }
+
+    public static void exitSystem(){
+        logger.severe("Shutting down application");
+        System.exit(1);
+    }
+
+    public static void close(ServerConnection connection){
+        if(connection != null){
+            try {
+                connection.close();
+            } catch (IOException e) {
+                logger.info("Error closing connection: " + e.getMessage());
+            }
+        }
+    }
+}
